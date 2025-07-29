d5e54e7809ee89ca941340555fea4364716baa8d
Lunyaa <77119458+LunyaaDev@users.noreply.github.com>
2025-07-29 05:17:16 +0200

    add comments to Instance interface

--- src/interfaces/Instance.d.ts
+++ src/interfaces/Instance.d.ts
@@ -1,6 +1,34 @@
 interface InstanceBase {
+  /**
+   * World ID of the instance.
+   */
   worldId: string
+  /**
+   * Name of the instance.
+   *
+   * Usually a 5 digit number, can also be a custom name.
+   *
+   * Max length is 41 characters.
+   *
+   * Allowed characters are:
+   * - a-z
+   * - A-Z
+   * - 0-9
+   * - _ (underscore)
+   * - \- (dash)
+   */
   name: string
+  /**
+   * Region of the instance.
+   *
+   * | Region | Hosted in | Token |
+   * | --- | --- | --- |
+   * | USA, West | San José | us |
+   * | USA, East | Washington D.C. | 	use |
+   * | Europe | Amsterdam | eu |
+   * | Japan | Tokyo | jp |
+   *
+   */
   region?: 'eu' | 'jp' | 'us' | 'use'
   /**
    * @deprecated Removed from VRChat instances
@@ -9,17 +37,39 @@ interface InstanceBase {
 }
 
 interface InstancePublic extends InstanceBase {
+  /**
+   * Type of the instance.
+   */
   type: 'public'
 }
 
 interface InstanceUser extends InstanceBase {
+  /**
+   * Type of the instance.
+   */
   type: 'friends+' | 'friends' | 'invite+' | 'invite'
+  /**
+   * User ID of the user who created the instance.
+   *
+   * @example usr_00000000-0000-0000-0000-000000000000
+   */
   userId: string
 }
 
 interface InstanceGroup extends InstanceBase {
+  /**
+   * Type of the instance.
+   */
   type: 'groupPublic' | 'group+' | 'group'
+  /**
+   * Group ID of the group who created the instance.
+   *
+   * @example grp_00000000-0000-0000-0000-000000000000
+   */
   groupId: string
+  /**
+   * Wether the group requires users to be 18 years or older to join.
+   */
   require18yo: boolean
 }
 
